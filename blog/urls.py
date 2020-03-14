bbd338b55f48542ef69aa3064a77731a37f03fe6
oyl1998 <1035127104@qq.com>
2020-03-14 16:38:40 +0800

    添加了Blog APP
    简单制作了Blog网页

--- blog/urls.py
+++ blog/urls.py
@@ -11,17 +11,12 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', path('blog/', include('blog.urls'))('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
-import blog.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home),
-    path('blog/', include('blog.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.blog_page),
+]
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # my APP
-    'gallery.apps.GalleryConfig',
+    'gallery',
+    'blog',
 ]
 
 MIDDLEWARE = [
